e8e08f3d83a884b9e1391a5b5597f55477c4677f
artick788 <60552965+artick788@users.noreply.github.com>
2024-11-30 13:55:29 +0100

    Fixed genre searcher

--- mp3mgr/SearchGenre.py
+++ mp3mgr/SearchGenre.py
@@ -14,12 +14,15 @@ def search_genre(artist: str, song_name: str) -> [str]:
 
     options = webdriver.ChromeOptions()
     options.add_extension(COOKIE_FOLDER)
-    options.add_argument("--start-maximized")
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--disable-dev-shm-usage")  # For resource constraints
+    options.add_argument("--no-sandbox")
+    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.199 Safari/537.36")
 
     driver = webdriver.Chrome(options=options)
     driver.get(base_url)
-
-    genre: [str] = []
     try:
         # Wait for the page to load
         search_input = WebDriverWait(driver, 15).until(
@@ -40,18 +43,19 @@ def search_genre(artist: str, song_name: str) -> [str]:
         )
         first_result.click()
 
-        # Wait for the page to load
-        spotify_box = WebDriverWait(driver, 15).until(
-            EC.presence_of_element_located((By.ID, "spotify-tags"))
+        # Wait for the spotify tags section to appear
+        spotify_result = WebDriverWait(driver, 15).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "spotify-result"))
         )
 
-        # The spotify box contains a list of genres
-        spotify_result = spotify_box.find_element(By.CLASS_NAME, "spotify-result")
-        tag_cloud = spotify_result.find_element(By.CSS_SELECTOR, ".p1-tags.tagcloud")
+        # Locate the tag cloud within the spotify-result
+        tag_cloud = spotify_result.find_element(By.CSS_SELECTOR, ".pl-tags.tagcloud")
 
+        # Extract all genre links
+        genre_elements = tag_cloud.find_elements(By.TAG_NAME, "a")
+        genres = [genre.text for genre in genre_elements]
 
-        print("Done")
-        time.sleep(5)
+        return genres
 
     except TimeoutException:
         print("[ChosicScraper]: Loading took too much time!")
@@ -60,11 +64,11 @@ def search_genre(artist: str, song_name: str) -> [str]:
         print("[ChosicScraper]: Element not found: " + str(e))
 
     driver.quit()
-    return genre
+    return []
 
 
 def main():
-    genre = search_genre("Skrillex", "Bangarang")
+    genre = search_genre("Tupac", "hit em up")
     print(genre)
 
 
